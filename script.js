473c35296aca37b18c5b9d5b84f2130395187c9e
jpleonardxyz <169338128+jpleonardxyz@users.noreply.github.com>
2024-10-09 09:23:13 +0200

    Add code for default grid and modify css

---
+++ script.js
@@ -0,0 +1,25 @@
+//define dimension of grid which does not change
+const gridDimension = 860;
+
+//define initial number of squares across and total squares
+let numSquaresAcross = 16;
+let totalSquares = numSquaresAcross**2;
+console.log(totalSquares);
+
+//obtain reference to container div
+const container = document.querySelector("#container");
+
+//calculate width of new squares, always round down to prevent overflow
+let squareWidth = Math.floor(gridDimension/numSquaresAcross);
+
+
+//create divs with correct size
+for (let i = 0; i<totalSquares; i++){
+    let square = document.createElement("div");
+    square.style.width = squareWidth + "px";
+    square.style.height = squareWidth + "px";
+    square.style.border = "2px solid grey";
+    square.style.boxSizing= "border-box";
+    container.appendChild(square);
+}
+
